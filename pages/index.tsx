d594e2db80385342e6bbf2bced25e52fa5d41744
Victor Costa <victorjosueldacosta@gmail.com>
2024-03-31 22:53:08 -0300

    Configuração incial do projeto, configurando arquivos e pastas. Instalação do sass e do bootstrap.

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss"
+
+const HomeNotAuth = function () {
+    return (
+        <>
+            <Head>
+                <title></title>
+            </Head>
+            <main>ON</main>
+        </>
+    )
+};
+
+export default HomeNotAuth;
