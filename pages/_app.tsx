2f9a3df13379d3ee224870448c9458429e7cb4f7
Victor Costa <victorjosueldacosta@gmail.com>
2024-04-01 00:07:45 -0300

    Criação do header da página homeNoAuth - Header Não autenticado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
-import "bootstrap/dist/css/bootstrap.min.css"
+import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
